caf9c397c02ec69ac949f7b0a5073e0668a2aea5
savrabh.flairlabs <savrabh.flairlabs@ext.idfy.com>
2022-09-24 15:19:28 +0530

    no match

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -1,7 +1,17 @@
+import "./NoMatch.css";
 const NoMatch = () => {
   return (
-    <div>
-      <h4>Hello NoMatch Found</h4>
+    <div className="no-match">
+      <h1>404</h1>
+      <h3>OPPS! PAGE NOT FOUND</h3>
+      <span>
+        Sorry, the page you're looking for doesn't exist. If you think something
+        is broken, report a problem
+      </span>
+      <div className="button">
+        <button className="shadow">RETURN HOME</button>
+        <button className="shadow">REPORT PROBLEM</button>
+      </div>
     </div>
   );
 };
