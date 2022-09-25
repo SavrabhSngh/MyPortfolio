66e01a188c56c29f902af782fd2cf9e283336c39
savrabh.flairlabs <savrabh.flairlabs@ext.idfy.com>
2022-09-25 19:50:03 +0530

    Router Fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Components from "./Components";
 import NoMatch from "./Components/NoMatch/NoMatch";
+import _ from "lodash";
 
 const App = () => {
+  let url,
+    route = "/";
   const handleContextMenu = (e) => {
     e.preventDefault();
   };
 
   useEffect(() => {
+    url = window.location.href;
+    if (_.isEqual(url, process.env.REACT_APP_GIT_URL)) {
+      route = "/MyPortfolio";
+    }
     window.addEventListener("contextmenu", handleContextMenu);
     return () => {
       window.removeEventListener("contextmenu", handleContextMenu);
@@ -18,7 +25,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Components />}></Route>
+        <Route exact path={route} element={<Components />}></Route>
         <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </Router>
